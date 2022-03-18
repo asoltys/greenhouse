664ae6bce51cc9781e2d833c6cc8626c1266a681
Adam Soltys <asoltys@gmail.com>
2022-03-17 20:21:41 -0700

    remove rates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,5 @@
-<script context="module">
-  import { get } from "$lib/api";
-  export async function load({ fetch, session }) {
-    const props = await get("/rate.json", fetch);
-    return { props };
-  }
-</script>
-
 <script>
   import LandingPage from "$components/LandingPage.svelte";
-  export let rate;
 </script>
 
 <LandingPage />
-
-<!-- <h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
